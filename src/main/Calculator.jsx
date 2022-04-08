cc7d71a4e9420efd8e0ec21f3de5759d5c2bb8d9
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-08 20:12:25 -0300

    criada estrutura do componente button

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Button } from '../components/Button/Button'
 import './Calculator.css'
 
 export function Calculator() {
   return (
     <div className='calculator'>
-       testando calculator comp
+       <Button label="AC" />
+       <Button label="/" />
+       <Button label="7" />
+       <Button label="8" />
+       <Button label="9" />
+       <Button label="X" />
+       <Button label="4" />
+       <Button label="5" />
+       <Button label="6" />
+       <Button label="-" />
+       <Button label="1" />
+       <Button label="2" />
+       <Button label="3" />
+       <Button label="+" />
+       <Button label="0" />
+       <Button label="." />
+       <Button label="=" />
     </div>
   )
 }

26ef4bbfa5df0af6358a501c5f17ddeb7261e171
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-08 20:21:10 -0300

    criação e estilização do componente display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Button } from '../components/Button/Button'
+import { Display } from '../components/Display/Display'
 import './Calculator.css'
 
 export function Calculator() {
   return (
     <div className='calculator'>
+       <Display value={100} />
        <Button label="AC" />
        <Button label="/" />
        <Button label="7" />

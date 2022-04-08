b8e9d4fe16069c7775b7cabbc2381bbd9a5931d6
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-08 19:58:37 -0300

    subindo arquivos iniciais do projeto

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Calculator.css'
+
+export function Calculator() {
+  return (
+    <div className='calculator'>
+       testando calculator comp
+    </div>
+  )
+}

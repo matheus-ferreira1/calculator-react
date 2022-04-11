ab938a0e2acda4a3dee9b8e2260ca83db1afb45c
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-11 17:12:21 -0300

    adicionada lógica que impede que usuario insira varios pontos ou operadores em sequencia

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -13,8 +13,14 @@ export function Calculator() {
          console.log(display)
       } else if (button === "AC") {
          setDisplay('')
-      } else if (button === "+" || button === "-" || button === "*" || button === "/") {
-         setDisplay(display + button)
+      } else if (button === "+" || button === "-" || button === "*" || button === "/" || button === ".") {
+         let lastChar = display.toString();
+
+         if (lastChar.slice(-1) === '+' || lastChar.slice(-1) === '-' || lastChar.slice(-1) === '*' || lastChar.slice(-1) === '/' || lastChar.slice(-1) === '.') {
+            button = ''
+          } else {
+            setDisplay(display + button)
+          }
       } else {
          setDisplay(display + button)
       }
